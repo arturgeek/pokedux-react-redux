60020fe70b0ba2f9cf4b2a77da35aa88419b61a1
Morales <HXXVLM@io.ad.gmfinancial.com>
2022-09-07 16:18:34 -0500

    Redux first stage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import PokemonList from './components/PokemonList';
 import logo  from './statics/logo.svg'
 import getPokemons from './api';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setPokemons as setPokemonsActions } from './actions';
 
-function App() {
-
-  const [ pokemons, setPokemons ] = useState([])
+function App( { pokemons, setPokemons } ) {
+  console.log("🚀 ~ file: App.js ~ line 12 ~ App ~ pokemons", pokemons)
 
   useEffect( () => {
     const fetchPokemons = async () => { 
@@ -22,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <Col span={4} offset={10}>
-        <img src={logo} alt="Pokedux" />
+        <img src={logo} alt="Pokedux" /> 
       </Col>
       <Col span={8} offset={8} >
         <Searcher />
@@ -32,4 +33,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch( setPokemonsActions(value) )
+});
+
+export default connect( mapStateToProps, mapDispatchToProps ) (App);

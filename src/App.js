f4f3d41a2fbdb4bbcf945bd611a1eb67c96d1f21
Andres Morales <arturgeek@gmail.com>
2022-09-12 16:53:06 -0500

    Initial redux toolkit change, pending favorite

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import { Col, Spin } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import logo  from './statics/logo.svg'
-import { getPokemons } from './api';
-import './App.css';
 import { useEffect } from 'react';
-import { getPokemonsWithDetails, setLoading } from './actions';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { fetchPokemonWithDetails } from './slices/dataSlice';
+import './App.css';
 
 function App() {
   
-  const pokemons = useSelector( (state) => state.getIn(["data", "pokemons"]), shallowEqual ).toJS();
-  const loading = useSelector( (state) => state.getIn(["ui", "loading"]) );
+  const pokemons = useSelector( (state) => state.data.pokemons, shallowEqual);
+  const loading = useSelector( (state) => state.ui.loading );
   const dispatch = useDispatch();
 
   useEffect( () => {
-    const fetchPokemons = async () => { 
-      dispatch( setLoading(true) )
-      const pokemonsResponse =  await getPokemons();
-      dispatch(getPokemonsWithDetails(pokemonsResponse));
-      dispatch(setLoading(false) )
-    };
-
-    fetchPokemons();
+    dispatch( fetchPokemonWithDetails() );
   }, [] )
 
   return (
---
+++ src/slices/dataSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getPokemonDetails, getPokemons } from "../api";
+import { setLoading } from "./uiSlice";
+
+const initialState = {
+    pokemons: []
+}
+
+export const fetchPokemonWithDetails = createAsyncThunk(
+    "data/fetchPokemonWithDetails",
+    async (_, {dispatch}) => {
+        dispatch( setLoading(true));
+        const pokemonsResponse = await getPokemons();
+        const pokemonsDetailed = await Promise.all( pokemonsResponse.map( (pokemon) => getPokemonDetails(pokemon) ));
+        dispatch( setPokemons(pokemonsDetailed) );
+        dispatch( setLoading(false));
+    }
+);
+
+export const dataSlice = createSlice({
+    name: "data",
+    initialState,
+    reducers: {
+        setPokemons: (state, action) => {
+            console.log(action);
+            state.pokemons = action.payload;
+        },
+        setFavorite: (state, action) => {
+            const currentPokemonIndex = state.pokemons.findIndex( (pokemon) => {
+                return pokemon.id === action.payload.pokemonId 
+            });
+
+            if( currentPokemonIndex >= 0 ){
+                const isFavorite = state.pokemons[currentPokemonIndex].favorite;
+                state.pokemons[currentPokemonIndex].favorite = !isFavorite;
+            }
+        }
+    }
+});
+
+export const { setFavorite, setPokemons } = dataSlice.actions;
+export default dataSlice.reducer;
